12873d13294f5f92ba0c4a7724c30144cf8325e2
Sigma <mukae.yuta.0811@gmail.com>
2025-12-12 15:12:42 +0900

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Sigma7863 on 2025/12/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTrackerTests.swift
+//  FoodTrackerTests
+//
+//  Created by Sigma7863 on 2025/12/12.
+//
+
+import Testing
+@testable import FoodTracker
+
+struct FoodTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
