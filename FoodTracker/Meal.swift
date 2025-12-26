ddbdde0c1c964907144c6d36fafe549b99417af6
Sigma <mukae.yuta.0811@gmail.com>
2025-12-26 14:40:58 +0900

    とりあえず最後まで出来た

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -6,12 +6,27 @@
 //
 
 import UIKit
+import os.log
 
-class Meal {
+class Meal: NSObject, NSSecureCoding {
     // MARK: Properties
     var name: String
     var photo: UIImage?
     var rating: Int
+    
+    // MARK: Archiving Paths
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
+    
+    // MARK: Types
+    // static キーワードはこのプロパティがこの構造体自身に所属しており、インスタンスではないことを表しています。
+    // そのためアクセスする際は構造体名に続けてプロパティ名を書くことになります。
+    //（例： PropertyKey.name）
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+    }
 
     // MARK: Initialization
     init?(name: String, photo: UIImage?, rating: Int) {
@@ -36,4 +51,31 @@ class Meal {
         self.photo = photo
         self.rating = rating
     }
+    
+    // MARK: NSCoding
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: PropertyKey.name)
+        coder.encode(photo, forKey: PropertyKey.photo)
+        coder.encode(rating, forKey: PropertyKey.rating)
+    }
+    
+    // 保存したデータを読み込む
+    required convenience init?(coder: NSCoder) {
+        // 名前は必須です。名前文字列をデコードできない場合、初期化は失敗します。(The name is required. If we cannot decode a name string, the initializer should fail.)
+        guard let name = coder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("Unable to decode the name for a Meal Object.", log: OSLog.default, type: .debug) // Mealオブジェクトの名前をデコードできません。
+            return nil
+        }
+            
+        // Because photo is an optional property of Meal, just use conditional cast.
+        let photo = coder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        
+        let rating = coder.decodeInteger(forKey: PropertyKey.rating)
+        
+        // 指定された初期化子を呼び出す必要があります。(Must call designated initializer.)
+        self.init(name: name, photo: photo, rating: rating)
+    }
+    
+    // MARK: NSSecureCoding
+    static var supportsSecureCoding: Bool = true
 }
--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -19,9 +19,17 @@ class MealTableViewController: UITableViewController {
         }
         
         super.viewDidLoad()
-        
-        // Load the sample data.
-        loadSampleMeals()
+
+        // テーブル ビュー コントローラによって提供される編集ボタン項目を使用します。(Use the edit button item provided by the table view controller.)
+        navigationItem.leftBarButtonItem = editButtonItem
+
+        // 保存した食事を読み込み、そうでない場合はサンプル データを読み込みます。(Load any saved meals, otherwise load sample data.)
+        if let savedMeals = loadMeals() {
+            meals += savedMeals
+        } else {
+            // Load the sample data.
+            loadSampleMeals()
+        }
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -42,7 +50,7 @@ class MealTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return meals.count
     }
-    
+
     // 列ごとに表示するセルを決めるデリゲートメソッド
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // セルを取得する(ユーザが Table View をスクロールすると新しいセルが作られ、見えなくなったセルが削除される代わりに Table View はセルを再利用しようとする, 再利用できない場合は新しくセルを作成する, これによりスクロールしてもアプリが重くなることなく利用できるようになっている。)
@@ -50,40 +58,39 @@ class MealTableViewController: UITableViewController {
         
         // テーブルビューのセルは再利用されるため、セル識別子を使用してキューから取り出す必要があります(Table view cells are reused and should be dequeued using a cell identifier)
         let cellIdentifier = "MealTableViewCell"
-        
+
         // UITableViewCell から MealTableViewCell へのダウンキャストが発生するので guard 文で早期リターンをさせる
         guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MealTableViewCell else {
             fatalError("The dequeued cell is not instance of MealTableViewCell.") // キューから削除されたセルは MealTableViewCell のインスタンスではありません。
         }
-            // データ ソース レイアウトに適したmealを取得します。(Fetches the appropriate meal for the data source layout.
-            let meal = meals[indexPath.row]
-            
-            cell.nameLabel.text = meal.name
-            cell.photoImageView.image = meal.photo
-            cell.ratingControl.rating = meal.rating
-            
-            return cell
+
+        // データ ソース レイアウトに適したmealを取得します。(Fetches the appropriate meal for the data source layout.
+        let meal = meals[indexPath.row]
+
+        cell.nameLabel.text = meal.name
+        cell.photoImageView.image = meal.photo
+        cell.ratingControl.rating = meal.rating
+
+        return cell
     }
 
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            meals.remove(at: indexPath.row)
+            saveMeals()
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
@@ -117,7 +124,6 @@ class MealTableViewController: UITableViewController {
         -----------------------
         """)
 
-        
         // segue.identifier の型は Optional<String> で nil になりえるが
         // 今回 nil の場合は想定していないので nil 結合演算子により nil の場合は空文字に変換して String 型を switch 文に渡している
         switch segue.identifier ?? "" {
@@ -127,23 +133,22 @@ class MealTableViewController: UITableViewController {
                 guard let mealDetailViewController = segue.destination as? MealDetailViewController else {
                     fatalError("Unexpected destination: \(segue.destination)") // 予期せぬ宛先:
                 }
-                
+
                 guard let selectedMealCell = sender as? MealTableViewCell else {
                     fatalError("Unexpected sender: \(String(describing: sender))") // 予期せぬ送信者:
                 }
-            
+
                 guard let indexPath = tableView.indexPath(for: selectedMealCell) else {
                     fatalError("The selected cell is not being displayed by the table") // 選択したセルはテーブルに表示されません
                 }
-            
+
                 let selectedMeal = meals[indexPath.row]
                 mealDetailViewController.meal = selectedMeal
-            
             default:
-                fatalError("Unexpected Seque Identifier; \(String(describing: segue.identifier))")
+                fatalError("Unexpected Segue Identifier; \(String(describing: segue.identifier))")
         }
     }
-    
+
     // MARK: Actions
     // MealViewController で作成された新しい Meal オブジェクトを MealTableViewController に追加し反映する
     @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
@@ -155,32 +160,63 @@ class MealTableViewController: UITableViewController {
             } else {
                 // Add a new meal.
                 let newIndexPath = IndexPath(row: meals.count, section: 0)
-                
+
                 meals.append(meal)
                 tableView.insertRows(at: [newIndexPath], with: .automatic) // TableViewに行を追加する
             }
+
+            // Save the meals.
+            saveMeals()
         }
     }
-    
+
     // MARK: Private Methods
     private func loadSampleMeals() {
         let photo1 = #imageLiteral(resourceName: "meal1")
         let photo2 = #imageLiteral(resourceName: "meal2")
         let photo3 = #imageLiteral(resourceName: "meal3")
-        
+
         guard let meal1 = Meal(name: "Hamburger", photo: photo1, rating: 4) else {
             fatalError("Unable to instantiate meal1") // meal1 をインスタンス化できません
         }
-        
+
         guard let meal2 = Meal(name: "Sushi Bento", photo: photo2, rating: 5) else {
             fatalError("Unable to instantiate meal2") // meal2 をインスタンス化できません
         }
-        
+
         guard let meal3 = Meal(name: "Grilled Beef", photo: photo3, rating: 3) else {
             fatalError("Unable to instantiate meal3") // meal3 をインスタンス化できません
         }
-        
+
         meals += [meal1, meal2, meal3]
+    }
+
+    private func saveMeals() {
+        // 保存できる形式にした Meal オブジェクトを適切な場所に保存する
+        guard let archivedData = try? NSKeyedArchiver.archivedData(withRootObject: meals, requiringSecureCoding: false) else {
+            fatalError("Unable to aechive meals") // mealsをアーカイブできません
+        }
+
+        // アーカイブしたデータをファイルに保存し、結果に対してログ出力をする
+        do {
+            try archivedData.write(to: Meal.ArchiveURL)
+            os_log("Meals successfully saved.", log: OSLog.default, type: .debug)
+        } catch {
+            os_log("Failed to save meals...", log: OSLog.default, type: .error)
+        }
+    }
+
+    private func loadMeals() -> [Meal]? {
+        do {
+            let data = try Data(contentsOf: Meal.ArchiveURL)
+            if let loadedData = try NSKeyedUnarchiver.unarchivedArrayOfObjects(ofClasses: [Meal.self, NSString.self, UIImage.self], from: data) as? [Meal] {
+                os_log("Meals successfully loaded.", log: OSLog.default, type: .debug)
+                return loadedData
+            }
+        } catch {
+            os_log("Failed to load meals...", log: OSLog.default, type: .error)
+        }
+        return nil
     }
 
 }
